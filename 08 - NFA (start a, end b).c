bcc472129aa5f1b76eb6def080bcc06f77915783
thanusthapasvi <123483866+thanusthapasvi@users.noreply.github.com>
2024-03-27 13:41:13 +0530

    Add files via upload

---
+++ 08 - NFA (start a, end b).c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+//   start with b and end with a
+int main()
+{
+	char s[10];
+	int i,k,c=0;
+	printf("Use Only a's or b's\n");
+	printf("Enter a string : ");
+	scanf("%s",s);
+	k=strlen(s);
+	for(i = 0;i<k;i++)
+    {
+    	if(s[i] == 'a' || s[i] == 'b')
+    		c = c+1;
+	}
+	if (c == k)
+	{
+		if(s[0]=='b' && s[k-1]=='a')
+			printf("\nString Accepted");
+		else
+			printf("\nNot Accepted");
+	}
+	else
+		printf("\nstring must be a's and b's");
+}
+
